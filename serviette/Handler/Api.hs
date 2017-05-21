2be7b92e0b77f18c0efba44db113394a28ddb467
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-05-21 22:32:31 +0200

    Add app types

--- serviette/Handler/Api.hs
+++ serviette/Handler/Api.hs
@@ -1,9 +1,52 @@
 module Handler.Api where
 
-import Import
+import qualified Data.Aeson as J
+import           Import
 
-getApiR :: Handler Html
-getApiR = error "Not yet implemented: getApiR"
+{-
+   received JSON example
 
-postApiR :: Handler Html
-postApiR = error "Not yet implemented: postApiR"
+   {
+     command : "SELECT | INSERT | UPDATE | DELETE",
+     tableName: "users",
+     join:[
+        {tableName:"contracts",field:"contractField",operator:"",withTable:"users", withField:"usersField"},
+        {tableName:"commissions",field:"contractField",operator:"",withTable:"contracts", withField:"usersField"}
+
+     ],
+     where:[
+        {tableName:"commissions",field:"contractField",operator:"", fieldValue:1}
+     ],
+     groupBy:[
+
+     ],
+     orderBy:[
+
+     ]
+
+
+   }
+
+-}
+
+-- | Types declaration
+type TableName  = String
+type ColumnName = String
+data FieldValue = Int | String
+data Operator   = Equals | NotEquals | LargerThan | LessThan | NotNull | Null
+data Command    = SELECT TableName | INSERT TableName | UPDATE TableName | DELETE TableName
+data JoinTable  = JoinTable TableName
+data Where      = Where TableName ColumnName Operator FieldValue
+data Groupby    = Groupby ColumnName
+data Orderby    = Orderby ColumnName
+
+data SqlSelectQuery = SqlSelectQuery Command (Maybe [JoinTable]) (Maybe Groupby) (Maybe Orderby)
+
+getApiR :: Handler Value
+getApiR = do
+  return $ J.String "get api"
+
+postApiR :: Handler Value
+postApiR = do
+  return $ J.String "post api"
+
--- serviette/Handler/Home.hs
+++ serviette/Handler/Home.hs
@@ -1,12 +1,10 @@
 module Handler.Home where
-import           Data.Aeson      (object, (.=))
-import qualified Data.Aeson      as J
-import           Data.Text       (pack)
+
+import qualified Data.Aeson as J
 import           Import
-import           Yesod.Core.Json (returnJson)
 
 getHomeR :: Handler Value
 getHomeR = do
-  return $ String "serviette"
+  return $ J.String "serviette"
 
 
