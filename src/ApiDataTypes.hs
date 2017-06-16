c892acf5899ac502d9a526eb47ddc1ae12152993
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-06-17 00:42:39 +0200

    Add haddock docs

--- src/ApiDataTypes.hs
+++ src/ApiDataTypes.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
+-- | This provides types that represent sql query structure
 module ApiDataTypes where
 
 import           Control.Applicative
@@ -13,26 +14,33 @@ import           Generics.Deriving
 
 -- | Type declaration
 
+-- | Represents db table name
 data TableName = TableName Text
   deriving (Show, Generic)
 
+-- | Represents db table column name
 data ColumnName = ColumnName Text
   deriving (Show, Generic)
 
+-- | Represents operator (=, >, <, like, not like etc.)
 data Operator = Operator Text
   deriving(Show, Generic)
 
+-- | Represents the main action for the sql query (SELECT,INSERT, UPDATE, DELETE)
 data Action = Action Text
   deriving (Show, Generic)
-
+-- | Represents return format (for now this is only raw sql string)
 data Format = Format
   { getFormat :: Int
   } deriving (Show, Eq)
 
+-- | Represents Date field value
 type DateVal = Text
 
+-- | Represents field value which can be integer, string or date
 data FieldValue = IntField Int | TextField Text | DateField DateVal deriving (Show, Generic)
 
+-- | Represents join table for the sql query
 data JoinTable = JoinTable
   { tablename          :: TableName
   , field              :: ColumnName
@@ -42,6 +50,7 @@ data JoinTable = JoinTable
   , whereConditionJoin :: Text
   } deriving (Show, Generic)
 
+-- | Represents main where condition
 data WhereCondition = WhereCondition
   { whereTableName  :: TableName
   , whereField      :: ColumnName
@@ -49,7 +58,7 @@ data WhereCondition = WhereCondition
   , whereFieldValue :: FieldValue
   } deriving (Show, Generic)
 
-
+-- | Represents intermediate type for receiving json
 data SqlQuery = SqlQuery
   { format         :: Int
   , action         :: Action
@@ -58,6 +67,7 @@ data SqlQuery = SqlQuery
   , whereCondition :: [WhereCondition]
   } deriving (Show, Generic)
 
+-- | Represents type that is the result of the json "computation"
 data SqlResultQuery = SqlResultQuery
   { getAction         :: Action
   , getSelectTable    :: TableName
@@ -68,31 +78,58 @@ data SqlResultQuery = SqlResultQuery
 
 -- | Instances
 
+-- | Json instance for TableName
 instance FromJSON TableName
+
+-- | Json instance for TableName
 instance ToJSON TableName
 
+-- | aeson instance for Action
 instance FromJSON Action
+
+-- | aeson instance for Action
 instance ToJSON Action
 
+-- | aeson instance for Operator
 instance FromJSON Operator
+
+-- | aeson instance for Operator
 instance ToJSON Operator
 
+-- | aeson instance for JoinTable
 instance FromJSON  JoinTable
+
+-- | aeson instance for JoinTable
 instance ToJSON  JoinTable
 
+-- | aeson instance for ColumnName
 instance FromJSON  ColumnName
+
+-- | aeson instance for ColumnName
 instance ToJSON ColumnName
 
+-- | aeson instance for SqlQuery
 instance FromJSON SqlQuery
+
+-- | aeson instance for SqlQuery
 instance ToJSON SqlQuery
 
+-- | aeson instance for SqlResultQuery
 instance FromJSON SqlResultQuery
+
+-- | aeson instance for SqlResultQuery
 instance ToJSON SqlResultQuery
 
+-- | aeson instance for FieldValue
 instance FromJSON FieldValue
+
+-- | aeson instance for FieldValue
 instance ToJSON FieldValue
 
+-- | aeson instance for WhereCondition
 instance ToJSON   WhereCondition
+
+-- | aeson instance for WhereCondition
 instance FromJSON WhereCondition where
     parseJSON (Object v)
         =  WhereCondition
@@ -105,4 +142,3 @@ instance FromJSON WhereCondition where
 
             )
     parseJSON _  = mzero
-
--- src/Serviette.hs
+++ src/Serviette.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- | Define functions needed for Json to Text manipulation
+-- current version supports only raw sql string result
+-- next version will implement database query result in the json format
+-- as well as errors in the json structure if any
 
 module Serviette (SqlQuery, SqlResultQuery, rawSqlStr) where
 
@@ -8,36 +12,47 @@ import           Data.Text    hiding (concat, map)
 
 -- | Various Getters
 
+-- | Extracts the action and appends space
 extractAction :: Action -> Text
 extractAction (Action t) = append t  " "
 
+-- | Extracts table name to Text
 extractTableName :: TableName -> Text
 extractTableName (TableName t) = t
 
+-- | Extracts table column name to Text
 extractColumnName :: ColumnName -> Text
 extractColumnName (ColumnName t) = t
 
+-- | Extracts Operator to Text
 extractOperator :: Operator -> Text
 extractOperator (Operator t) = t
 
+-- | Fetches the Action from SqlQuery
 getActionArg :: SqlQuery -> Action
 getActionArg q = action q
 
+-- | Gets the main table that Action is performed on
 getSelectTableArg :: SqlQuery -> TableName
 getSelectTableArg q = selectName q
 
+-- | Retrieves the join list from the SqlQuery
 getJoinTableArg :: SqlQuery -> [JoinTable]
 getJoinTableArg q =  joinTables q
 
+-- | Gets the where condition list
 getWhereConditionArg :: SqlQuery -> [WhereCondition]
 getWhereConditionArg q =  whereCondition q
 
+-- | Retrieves the result Format
 getFormatArg :: SqlQuery -> Int
 getFormatArg q =  getFormat $ Format $ format q
 
+-- | Formats the join table list
 formatJoinStr :: JoinTable -> Text
 formatJoinStr j = Prelude.foldl append "" (" join " : [(extractTableName $ tablename j) , " on " , (extractColumnName $ field j) , " " ,(extractOperator $ operator j) , " " , (extractTableName $ withTable j)  , "." , (extractColumnName $ withField j), " " ])
 
+-- | Fetches field value depending on field type
 formatFieldValue :: FieldValue -> String
 formatFieldValue a =
   case a of
@@ -45,12 +60,15 @@ formatFieldValue a =
     TextField x -> show x
     DateField x -> show x
 
+-- | Formats WhereCondition to Text
 formatWhereConditionStr :: WhereCondition -> Text
 formatWhereConditionStr j = Prelude.foldl append " " (" where " : [ (extractTableName $ whereTableName j), "." , (extractColumnName $ whereField j) , " " ,(extractOperator $ whereOperator j) , " " , (pack $ formatFieldValue $ whereFieldValue j)])
 
 
+-- | Creates final SqlResultQuery type
 formatToSqlResultQueryType sql = SqlResultQuery (getActionArg sql) (getSelectTableArg sql) (getJoinTableArg sql) (getWhereConditionArg sql)
 
+-- | Returns raw sql query string
 rawSqlStr :: SqlQuery -> Text
 rawSqlStr s =
   Prelude.foldl append "" [(extractAction $ getAction sql) ,(extractTableName $ getSelectTable sql) , joins , whereConditions ]
