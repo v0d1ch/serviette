f829f52a317bddea08ae327fe752fb848d7e6a19
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-06-23 00:02:54 +0200

    Add comment

---
+++ Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Data.ApiDataTypes
+import Data.Serviette
+import Data.Aeson
+import Data.Text
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+
+-- | THIS FILE IS HERE JUST FOR TESTING PURPOSES !!!
+
+jsonFile :: FilePath
+jsonFile = "test/delete_data.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+
+main :: IO ()
+main = do
+ d <- (eitherDecode <$> getJSON) :: IO (Either String SqlQuery)
+ case d of
+  Left err -> putStrLn err
+  Right ps -> print $ rawSqlStr ps
