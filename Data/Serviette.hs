439eff1587707f0c3f54bfe57796677f1c9e0eb2
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-06-24 21:49:48 +0200

    Add errors and warnings

--- Data/Serviette.hs
+++ Data/Serviette.hs
@@ -102,29 +102,44 @@ formatToSqlResultQueryType sql = SqlResultQuery (getActionArg sql) (getSelectTab
 
 getErrors :: SqlQuery -> Text
 getErrors s = t
-  where t = case action s of
-                Action "SELECT" ->
-                    if isJust (set s) then " Do not use SET in SELECT query " else ""
-                    -- if  selectName s == "" then " You are missing the FROM table name in SELECT statement " else  ""
-                Action "DELETE" ->
-                  if isNothing (joinTables s) then "" else " Do not use joins in DELETE query "
-                Action "UPDATE" ->
-                  if isJust (joinTables s) then " Do not use joins in UPDATE query " else ""
-                Action "INSERT" ->
-                  if isJust (joinTables s) then " Do not use joins in UPDATE query " else ""
+  where
+    t =
+      case action s of
+        Action "SELECT"
+          | isJust (set s) -> " Do not use SET in SELECT query "
+          | Data.Text.null (extractTableName $ selectName s)  -> " You are missing the FROM table name in SELECT statement "
+          | otherwise -> ""
+        Action "DELETE"
+          | isJust (joinTables s) -> " Do not use joins in DELETE query "
+          | otherwise -> ""
+        Action "UPDATE"
+          | isJust (joinTables s) -> " Do not use joins in UPDATE query "
+          | otherwise -> ""
+        Action "INSERT"
+          | isJust (joinTables s) -> " Do not use joins in UPDATE query "
+          | otherwise -> ""
+
 
 
 getWarnings :: SqlQuery -> Text
-getWarnings s =
+getWarnings s = t
+  where
+    t =
       case action s of
-        Action "SELECT" ->
-          if isNothing (whereCondition s) then " You are probably missing WHERE statement " else ""
-        Action "DELETE" ->
-          if isNothing (whereCondition s) then " You are probably missing WHERE statement " else ""
-        Action "UPDATE" ->
-          if isNothing (whereCondition s) then " You are missing WHERE statement " else ""
-        Action "INSERT" ->
-          if isNothing (set s) then " You are missing the SET statement " else ""
+        Action "SELECT"
+          | isNothing (whereCondition s) ->
+            " You are probably missing WHERE statement "
+          | otherwise -> ""
+        Action "DELETE"
+          | isNothing (whereCondition s) ->
+            " You are probably missing WHERE statement "
+          | otherwise -> ""
+        Action "UPDATE"
+          | isNothing (whereCondition s) -> " You are missing WHERE statement "
+          | otherwise -> ""
+        Action "INSERT"
+          | isNothing (set s) -> " You are missing the SET statement "
+          | otherwise -> ""
 
 
 
